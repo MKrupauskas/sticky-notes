24b02bcd58ce6e60bbd66e06d1a73e1aecda90de
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-08-30 22:27:32 +0300

    Update Board element

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,38 @@
 import Note from "note.js";
+
 class Board extends React.Component() {
+  getInitialState() {
+    return {
+      notes: ["Call Bill", "Email Lisa", "Finish Project"]
+    };
+  }
+  update(newText, i) {
+    var array = this.getInitialState.notes;
+    arr[i] = newText;
+    this.setState({ notes: array });
+  }
+  remove() {
+    var array = this.getInitialState.notes;
+    array.splice(i, 1);
+    this.setState({ notes: array });
+  }
+  eachNote(note, i) {
+    return (
+      <Note
+        key={i}
+        index={i}
+        onChange={this.update.bind(this)}
+        onRemove={this.remove.bind(this)}
+      >
+        {note}
+      </Note>
+    );
+  }
   render() {
     return (
-      <div className="Board">
-        {props.propName}
+      <div className="board">
+        {this.getInitialState.note.map(
+          this.eachNote.bind(this)}
       </div>
     );
   }
