bcfc658a0d7b3778a838e6ed7bf0dc96c277d82c
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-10-08 19:41:00 +0300

    fix broken Board component

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,88 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Note from "./note.js";
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editing: false };
+  }
+
+  edit = () => {
+    this.setState({ editing: true });
+  };
+
+  save = () => {
+    var value = this.refs.newText.getDOMNode().value;
+    alert("save note value " + value);
+    this.setState({ editing: false });
+  };
+
+  remove = () => {};
+
+  renderDisplay() {
+    return (
+      <div className="note">
+        <p>{this.props.children}</p>
+        <span>
+          <button
+            onClick={this.edit}
+            className="btn btn-primary glyphicon glyphicon-pencil"
+          />
+          <button
+            onClick={this.remove}
+            className="btn btn-danger glyphicon glyphicon-trash"
+          />
+        </span>
+      </div>
+    );
+  }
+
+  renderForm() {
+    return (
+      <div className="note">
+        <textarea
+          ref="newText"
+          defaultValue={this.props.children}
+          className="form-control"
+        />
+        <button
+          onClick={this.save}
+          className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk"
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return this.state.editing ? this.renderForm() : this.renderDisplay();
+  }
+}
 
 class Board extends Component {
-  getNotes() {
-    return {
+  constructor(props) {
+    this.state = {
       notes: ["Call Bill", "Email Lisa", "Finish Project"]
     };
   }
-  update(newText, i) {
-    var array = this.getNotes.notes;
-    arr[i] = newText;
+  update = (newText, i) => {
+    var array = this.state.notes;
+    array[i] = newText;
     this.setState({ notes: array });
-  }
-  remove() {
-    var array = this.getNotes.notes;
+  };
+  remove = () => {
+    var array = this.state.notes;
     array.splice(i, 1);
     this.setState({ notes: array });
-  }
+  };
   eachNote = (note, i) => {
     return (
-      <Note
-        key={i}
-        index={i}
-        onChange={this.update.bind(this)}
-        onRemove={this.remove.bind(this)}
-      >
+      <Note key={i} index={i} onChange={this.update} onRemove={this.remove}>
         {note}
       </Note>
     );
   };
   render() {
-    return <div className="board">{this.getNotes()}</div>;
+    return <div className="board">{this.state.notes.map(this.eachNote)}</div>;
   }
 }
 
